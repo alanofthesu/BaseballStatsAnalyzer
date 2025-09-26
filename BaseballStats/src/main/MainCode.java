2bb8b2dbe1dd7ded220def176350a2e891e98bcc
alanofthesu <alanatorsu@gmail.com>
2025-09-25 18:14:38 -0700

    initial commit

---
+++ BaseballStats/src/main/MainCode.java
@@ -0,0 +1,47 @@
+package main;
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class MainCode {
+
+	
+	public static void main(String[] args) {
+		
+		Scanner console = new Scanner(System.in);
+		System.out.println("pitch type: ");
+		String pitchType = console.nextLine();
+		System.out.println("velocity: ");
+		Double velocity = Double.parseDouble(console.nextLine());
+		System.out.println("spin rate: ");
+		Double spinRate = Double.parseDouble(console.nextLine());
+		System.out.println("h-break: ");
+		Double hb = Double.parseDouble(console.nextLine());
+		System.out.println("ivb: ");
+		Double ivb = Double.parseDouble(console.nextLine());
+		
+		System.out.println("velocity: " + velocity + "\nspin rate: " + spinRate + "\nh-break: " + hb + "\nivb: "+ivb);
+		console.close();
+		
+		if(pitchType.equals("4seam")) {
+			Parse4SeamData data = new Parse4SeamData("4seam new.csv","fastball stuff value new.csv");
+	    	System.out.print("closest pitcher match for 4seam fastball: ");
+			System.out.println(Arrays.toString(data.findClosestMatch(velocity, spinRate, hb, ivb)));
+			System.out.println("stuff value: " + data.getStuff());
+		}
+		else if (pitchType.equals("changeup")) {
+			ParseChangeupData data = new ParseChangeupData("changeup.csv","ch stuff value.csv");
+	    	System.out.print("closest pitcher match for changeup: ");
+			System.out.println(Arrays.toString(data.findClosestMatch(velocity, spinRate, hb, ivb)));
+			System.out.println("stuff value: " + data.getStuff());
+		
+		}
+		else
+		{
+			System.out.println("butt");
+		}
+		}
+	
+	
+
+}
